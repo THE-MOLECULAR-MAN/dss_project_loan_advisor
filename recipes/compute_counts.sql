bc4db3480d87fd61cec2e293b68b70cf8022a870
admin <admin>
2025-03-03 18:51:12 +0000

    Saved recipe: FINAIADVISOR.compute_counts

--- recipes/compute_counts.sql
+++ recipes/compute_counts.sql
@@ -1,2 +1,14 @@
-SELECT *
-  FROM "SEDEMO"."ERIND"."FINAIADVISOR_LABELED_SCORED_STACKED"
+-- To get the count of customers by loan purpose for California, we need to filter the records where 
+-- the 'State' is 'California'. Then, we group the results by 'Loan_Purpose' and count the number of 
+-- records in each group. The relevant columns for this query are 'State' and 'Loan_Purpose'.
+SELECT
+    "Loan_Purpose",
+    COUNT(*) AS "Customer_Count"
+FROM
+    "SEDEMO"."ERIND"."FINAIADVISOR_LABELED_SCORED_STACKED"
+WHERE
+    "State" = 'California'
+GROUP BY
+    "Loan_Purpose"
+ORDER BY
+    "Customer_Count" DESC;
